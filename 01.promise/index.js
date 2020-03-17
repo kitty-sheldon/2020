18249ba24b68efff0252798960e604c7f2aad270
zhangerjiang <erjiang.zhang@ymm56.com>
2020-03-17 08:11:02 +0800

    format

--- 01.promise/index.js
+++ 01.promise/index.js
@@ -1,21 +1,21 @@
-class Promise{
-    constructor(exector){
+class Promise {
+    constructor(exector) {
         this.state = 'pending'
         this.value = undefined
         this.reason = undefined
         this.onFulfilledCallbacks = []
         this.onRejectedCallbacks = []
 
-        const resolve = value =>{
+        const resolve = value => {
             this.state = 'fulfilled'
             this.value = value
-            this.onFulfilledCallbacks.forEach(fn=>fn())
+            this.onFulfilledCallbacks.forEach(fn => fn())
         }
 
-        const reject = reason =>{
+        const reject = reason => {
             this.state = 'rejected'
             this.reason = reason
-            this.onRejectedCallbacks.forEach(fn=>fn())
+            this.onRejectedCallbacks.forEach(fn => fn())
         }
 
         try {
@@ -24,13 +24,13 @@ class Promise{
             reject(error)
         }
     }
-    then(onFulfilled, onRejected){
-        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val=>val
-        onRejected = typeof onRejected === 'function' ? onRejected : err => {throw err}
+    then(onFulfilled, onRejected) {
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
+        onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
 
-        const promise2 = new Promise((resolve, reject)=>{
-            const asyncFulfilled = ()=>{
-                setTimeout(()=>{
+        const promise2 = new Promise((resolve, reject) => {
+            const asyncFulfilled = () => {
+                setTimeout(() => {
                     try {
                         const x = onFulfilled(this.value)
                         resolvePromise(promise2, x, resolve, reject)
@@ -40,8 +40,8 @@ class Promise{
                 })
             }
 
-            const asyncRejected = ()=>{
-                setTimeout(()=>{
+            const asyncRejected = () => {
+                setTimeout(() => {
                     try {
                         const x = onRejected(this.reason)
                         resolvePromise(promise2, x, resolve, reject)
@@ -51,7 +51,7 @@ class Promise{
                 })
             }
 
-            switch(this.state){
+            switch (this.state) {
                 case 'pending':
                     this.onFulfilledCallbacks.push(asyncFulfilled)
                     this.onRejectedCallbacks.push(asyncRejected)
@@ -66,91 +66,100 @@ class Promise{
         })
         return promise2
     }
-    catch(fn){
+    catch(fn) {
         this.then(null, fn)
     }
 }
 
-Promise.resolve = value =>{
-    return new Promise(resolve=>resolve(value))
-}
-
-Promise.reject = error =>{
-    return new Promise((resolve,reject)=>reject(error))
-}
-
-Promise.race = promises =>{
-    if(!Array.isArray(promises)){
-        return new TypeError('Promise.race arguments must be array')
+function resolvePromise(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+        return reject(new TypeError('循环引用'))
     }
-    return new Promise((resolve, reject)=>{
+    if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+        let called = false
+        try {
+            const { then } = x
+            if (typeof then === 'function') {
+                then.call(x, y => {
+                    if (called) return
+                    called = true
+                    resolvePromise(promise2, y, resolve, reject)
+                }, error => {
+                    if (called) return
+                    called = true
+                    reject(error)
+                })
+            } else {
+                resolve(x)
+            }
+        } catch (error) {
+            if (called) return
+            called = true
+            reject(error)
+        }
+    } else {
+        resolve(x)
+    }
+}
+
+Promise.resolve = value => {
+    return new Promise(resolve => resolve(value))
+}
+
+Promise.reject = error => {
+    return new Promise((resolve, reject) => reject(error))
+}
+
+Promise.race = promises => {
+    if (!Array.isArray(promises)) {
+        throw new TypeError('Promise.race arguments must be array')
+    }
+    return new Promise((resolve, reject) => {
         let length = promises.length
-        for(let i = 0; i < length; i++){
+        for (let i = 0; i < length; i++) {
             promises[i].then(resolve, reject)
         }
     })
 }
 
 Promise.all = promises => {
-    if(!Array.isArray(promises)){
-        return new TypeError('Promise.all arguments must be array')
+    if (!Array.isArray(promises)) {
+        throw new TypeError('Promise.all arguments must be array')
     }
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         let length = promises.length
         let result = []
         let num = 0
-        for(let i = 0; i < length; i++){
-            promises[i].then(data=>{
+        for (let i = 0; i < length; i++) {
+            promises[i].then(data => {
                 num++
                 result[i] = data
-                if(num === length) resolve(result)
+                if (num === length) resolve(result)
             }, reject)
         }
     })
 }
 
-function resolvePromise(promise2, x, resolve, reject){
-    if(promise2 === x){
-        return reject(new TypeError('循环引用'))
-    }
-    if(x != null && (typeof x === 'object' || typeof x === 'function')){
-        let called = false
-        try {
-            const {then} = x
-            if(typeof then === 'function'){
-                then.call(x, y=>{
-                    if(called) return
-                    called = true
-                    resolvePromise(promise2, y, resolve, reject)
-                }, error=>{
-                    if(called) return
-                    called = true
-                    reject(error)
-                })
-            }else{
-                resolve(x)
-            }
-        } catch (error) {
-            if(called) return
-            called = true
-            reject(error)
-        }
-    }else{
-        resolve(x)
-    }
-}
 
+//验证promise.all
+let p1 = Promise.resolve(1),
+    p2 = Promise.resolve(2),
+    p3 = Promise.resolve(3);
 
+Promise.all([p1, p2, p3]).then((res) => {
+    console.log(res, 'res')
+}, (err) => {
+    console.log(err, 'err')
+})
 
 // promises-aplus-tests 测试代码
 Promise.defer = Promise.deferred = function () {
     let dfd = {}
-    dfd.promise = new Promise((resolve,reject)=>{
-      dfd.resolve = resolve;
-      dfd.reject = reject;
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
     });
     return dfd;
-  }
+}
 module.exports = Promise;
 
-  
