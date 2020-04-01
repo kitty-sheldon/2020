95edce7e5429aaf1026feae563d6f576ac81eb71
chixiaoxiao <chixiaoxiao@jd.com>
2020-04-01 13:10:54 +0800

    fix:刷新num+1

--- 08.cross-tab/index.js
+++ 08.cross-tab/index.js
@@ -3,14 +3,16 @@
     
     document.getElementById('broadcast').onclick = (e)=>{
         const value = getValue(e)
+        const tab = getTab()
         bc.postMessage({
-            value
+            value,
+            tab
         })
         
     }
     bc.onmessage = (res)=>{
-        document.getElementById('bc-message').innerText = `[message]: ${res.data.value}`
-        console.log(res)
+        const {value, tab} = res.data;
+        document.getElementById('bc-message').innerText = `[message]: get ${value} from ${tab}`
     }
 })()
 
@@ -18,10 +20,15 @@
 window.onload = ()=>{
     const {type} = performance.navigation
     const isRefresh = type === 1
-    let num = Number(localStorage.getItem('xx_broad')) || 0
-    if((num && !isRefresh) || !num){
+    let num = /new/.test(location.search) ? Number(localStorage.getItem('xx_broad')) : 1
+    
+    //刷新不更改数字
+    if((num && !isRefresh) || !num ){
         num++
-        localStorage.setItem('xx_broad', num)
     }
+    localStorage.setItem('xx_broad', num)
     document.getElementById('title').innerText = `Tab${num}` 
+}
+function onpenNewTab(){
+    window.open(`${location.href}?new=1`)
 }
--- 08.cross-tab/util.js
+++ 08.cross-tab/util.js
@@ -1,3 +1,6 @@
 const getValue = (e)=>{
   return e.target.previousSibling.value
 }
+const getTab = ()=>{
+  return document.getElementById('title').innerText
+}
